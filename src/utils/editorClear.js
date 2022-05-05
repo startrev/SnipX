aadd1792c0e5bc7b8e1ce64141d004ffd33d95cf
arakilian0 <arakilian0@gmail.com>
2022-05-05 16:21:19 -0700

    ADD editor clear method

--- src/utils/editorClear.js
+++ src/utils/editorClear.js
@@ -1,3 +1,12 @@
 export default function editorClear(e) {
-    console.log(e.target.innerText)
+    chrome.storage.local.get(function(result) {
+        // Check if on valid url(browser specific urls are considered invalid)
+        if(!result.tabURL.includes(':') && document.querySelector('textarea')) {
+            // If text editor data doesn't = default value
+            if(document.querySelector('textarea')) {
+                document.querySelector('textarea').value = ''
+                document.querySelector('#editorSave').click()
+            }
+        }
+    })
 }
