fac4ff210be057e1205c7726a8a1ee781cdb67b5
arakilian0 <arakilian0@gmail.com>
2022-05-12 08:51:10 -0700

    feat(keybind): create Save Keybind(ctrl+s)

--- src/utils/editorShortcuts.js
+++ src/utils/editorShortcuts.js
@@ -2,5 +2,16 @@ chrome.storage.local.get(function(result) {
     document.addEventListener('keydown', e => {
         if(result.editorLock) { e.preventDefault() }
         if(e.key == 'Tab') { e.preventDefault() }
+        if(e.key == 'Control') {
+            document.addEventListener('keydown', _e => {
+                if(_e.key == 's') { 
+                    if(document.querySelector('#editorSave')) {
+                        _e.preventDefault()
+                        document.querySelector('#editorSave').click()
+                        window.location.reload()
+                    }
+                }
+            })
+        }
     })
 })
