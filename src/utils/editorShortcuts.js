15650e6ebc4cd39428337ba1d3da746794164fb5
arakilian0 <arakilian0@gmail.com>
2022-05-13 16:48:06 -0700

    refact(keybind): disabled keybinds

--- src/utils/editorShortcuts.js
+++ src/utils/editorShortcuts.js
@@ -1,17 +1,17 @@
 chrome.storage.local.get(function(result) {
     document.addEventListener('keydown', e => {
-        if(result.editorLock) { e.preventDefault() }
-        if(e.key == 'Tab') { e.preventDefault() }
-        if(e.key == 'Control') {
-            document.addEventListener('keydown', _e => {
-                if(_e.key == 's') { 
-                    if(document.querySelector('#editorSave')) {
-                        _e.preventDefault()
-                        document.querySelector('#editorSave').click()
-                        window.location.reload()
-                    }
-                }
-            })
-        }
+        // if(result.editorLock) { e.preventDefault() }
+        // if(e.key == 'Tab') { e.preventDefault() }
+        // if(e.key == 'Control') {
+        //     document.addEventListener('keydown', _e => {
+        //         if(_e.key == 's') { 
+        //             if(document.querySelector('#editorSave')) {
+        //                 _e.preventDefault()
+        //                 document.querySelector('#editorSave').click()
+        //                 window.location.reload()
+        //             }
+        //         }
+        //     })
+        // }
     })
 })
